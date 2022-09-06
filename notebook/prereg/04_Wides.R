748eec45ab8c9b57f816526ce5e0e5ea70d00faf
Benjamin Marshall <b.m.marshall@stir.ac.uk>
2022-09-06 15:27:26 +0100

    Area function built and sampling functions drafted
    Draft sampling code created with code to output subsampled datasets ready for build_available_areas.

---
+++ notebook/prereg/04_Wides.R
@@ -0,0 +1,82 @@
+library(here)
+library(multiverseHabitat)
+
+movementData <- read.csv(here("notebook", "prereg", "prelimData.csv"))
+movementData <- movementData[seq(0, 100000, by = 100),]
+movementData$datetime <- as.POSIXct(movementData$timestep * 60,
+           origin = "2022-01-01")
+
+library(raster)
+load(file = here("notebook", "prereg", "landscapePrelim.RData"))
+classRaster <- raster(classLandscapeList$classified, crs = sp::CRS(SRS_string = "EPSG:32601"),
+                      xmn = 0, xmx = ncol(classLandscapeList$classified),
+                      ymn = 0, ymx = nrow(classLandscapeList$classified))
+
+out <- build_available_area(movementData = movementData,
+                     method = "dBBMM",
+                     contour = 90)
+sp::plot(out)
+
+### POSSIBLE NEW NODE, RANDOM VERSUS SYSTEMATIC???
+availPoints <- spsample(out, n = 1000, type = "random")
+
+availValues <- raster::extract(classRaster, availPoints)
+availValues_DF <- data.frame(rbind(table(availValues)))
+names(availValues_DF) <- c("c0", "c1", "c2")
+
+availVector <- as.vector(table(availValues))
+names(availVector) <- c("c0", "c1", "c2")
+
+library(adehabitatHS)
+
+usedValues <- raster::extract(classRaster, sp::SpatialPoints(movementData[,c("x", "y")],
+                                                             sp::CRS(SRS_string = "EPSG:32601")))
+usedValues <- data.frame(rbind(table(usedValues)))
+names(usedValues) <- c("c0", "c1", "c2")
+
+# for widesI and widesII, a vector with named elements describing the sample or
+# the proportion of available resource units. For widesIII a matrix or a data
+# frame giving the number or the proportion of available resource units for each
+# animal (in rows) in each resource category (in columns)
+
+# so because the
+# difference is use the available, we can do III design with the II set up just
+# with different availabilities
+wiOUT <- widesIII(u = usedValues, a = availValues_DF)
+
+wiOUT$wi[3]
+
+# adehabitatHS::compana(used = usedValues, avail = availValues_DF)
+#
+# data(squirrel)
+#
+# ## Second order habitat selection
+# ## Selection of home range within the
+# ## study area
+# squiana2 <- compana(squirrel$mcp, squirrel$studyarea)
+# squiana2
+#
+# ## The ranking matrix:
+# print(squiana2$rm, quote = FALSE)
+#
+# compOUT <- adehabitatHS::eisera(used = usedValues, available = availValues_DF,
+#                                 nf = 4, scannf = FALSE)
+#
+# compOUT
+#
+# data(squirrel)
+#
+# ## computation of the number of relocations in each habitat type
+# ## from the data given by Aebischer et al. (1993).
+# ## squirrel$locs give the percentage of relocations in each habitat
+# ## type, and Aebischer et al. (1993) indicate that there are 30
+# ## relocations per animal.
+# ## We therefore compute the number of relocations in each habitat type
+# ## using:
+# us <- round(30 * squirrel$locs / 100)
+#
+# ## Habitat availability
+# av <- squirrel$studyarea
+#
+# ## Eigenanalysis of selection ratios
+# ii <- eisera(us, av, scannf = FALSE)
---
+++ notebook/prereg/Coding Plan.R
@@ -0,0 +1,27 @@
+### CODE STRUCTURE / WORKFLOW
+
+# Simulate landscapes - store outputs, 3, 1 for each species
+
+# Simulate population - 200? individuals, store all as separate RData files
+
+# Sampling - each individual gets all sampling nodes completed on them, so we
+# have 200 x node1 x node2 data files
+
+# Generate areas - as the areas will be reused and are likely a more
+# computationally intensive bit we can run 90,95,99 contours or each method for
+# each data file. store all outputs
+
+# Individual analyses - run SSF RSF and Wides designs
+
+# Population analyses - summarise SSF RSF and WIDES to pop level, then introduce
+# popSSF, Compana and Eisera. The summaries and pop analyses will need varying
+# sample sizes but that should be easy because the outputs will be stored.
+# Questions over repeats? but will be computationally low cos the areas etc are
+# already calculated and tracking data already sub sampled.
+
+## Overall this plan will use up loads of harddrive space, but will save
+## computational time. Maximise the data shared at the end, perhaps, polygons
+## and original sim data as those will likely take the longest to run.
+## Subsampling is more a convenience for later summaries and makes things nicely
+## clear for running analysis functions, it won't save much time besides not
+## needing to load in the largest tracking dataset files.
