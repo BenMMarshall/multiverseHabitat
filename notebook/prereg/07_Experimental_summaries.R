d193f312aab04c313a42db6748d9134b1c3f2b35
Benjamin Marshall <b.m.marshall@stir.ac.uk>
2022-12-19 11:42:51 +0000

    Updated summary plots for showing all results from indis

--- notebook/prereg/07_Experimental_summaries.R
+++ notebook/prereg/07_Experimental_summaries.R
@@ -64,16 +64,69 @@ combinedResults$sigColour <-
 combinedResults$sigColour
 
 library(ggplot2)
+library(dplyr)
+library(ggridges)
 
-ggplot(combinedResults) +
+palette <- c("#AD6DED", "#7D26D4", "#4F0E99", "#E87D13", "#965A1D", "#302010", "#403F41")
+names(palette) <- c("KING\nCOBRA", "VULTURE", "BADGER", "2", "1", "0", "coreGrey")
+
+combinedResults %>%
+  arrange(td) %>%
+  # mutate(branches = factor(branches,
+  #                          levels = branches)) %>%
+  ggplot() +
   # geom_segment(aes(x = Estimate - SE, xend = Estimate + SE,
   #                  y = branches, yend = branches, colour = sigColour)) +
-  geom_point(aes(x = Estimate, y = branches, colour = sigColour)) +
+  geom_point(aes(x = Estimate, y = td, colour = sigColour),
+             position = position_jitter(height = 0.1)) +
+  geom_vline(xintercept = 0, linewidth = 0.5, alpha = 0.45, colour = "#403F41",
+             linetype = 2) +
   facet_wrap(.~analysis, ncol = 1, scales = "free") +
-  scale_colour_manual(values = c("red", "grey25", "blue"), na.value = "black") +
+  scale_colour_manual(values = c("red", "grey25", "blue"), na.value = "#403F41") +
   theme_bw() +
-  theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        panel.grid = element_blank())
+  theme(
+    line = element_line(colour = palette["coreGrey"]),
+    text = element_text(colour = palette["coreGrey"]),
+    # axis.text.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    panel.grid = element_blank())
 
-order(combinedResults$branches, combinedResults$indi)
+# remove the spcies and indi from the branches as they are acting as our repeats,
+# that way each branch (y axis) has species*indi points
+combinedResults$choices <- sub("badger|vulture|kingcobra", "", combinedResults$branches)
+combinedResults$choices <- sub(".{3}$", "", combinedResults$choices)
+
+combinedResults %>%
+  group_by(choices) %>%
+  mutate(meanEst = mean(Estimate, na.rm = TRUE),
+         minEst = min(Estimate, na.rm = TRUE),
+         maxEst = max(Estimate, na.rm = TRUE),
+         analysis = case_when(
+           analysis == "wides" ~ "Wides",
+           analysis == "rsf" ~ "Resource Selection Function",
+           analysis == "ssf" ~ "(Integrated) Step Selection Function",
+           TRUE ~ analysis)
+         ) %>%
+  ggplot(aes(y = reorder(choices, Estimate, FUN = mean, na.rm = TRUE))) +
+  geom_segment(aes(x = minEst, xend = maxEst, yend = choices),
+               alpha = 0.1) +
+  geom_point(aes(x = Estimate, colour = sigColour),
+             position = position_jitter(height = 0.01, width = 0),
+             size = 0.75) +
+  geom_point(aes(x = meanEst,)) +
+  geom_vline(xintercept = 0, linewidth = 0.5, alpha = 0.45, colour = "#403F41",
+             linetype = 2) +
+  facet_wrap(.~analysis, ncol = 1, scales = "free") +
+  scale_colour_manual(values = c("#E87D13", "#403F41", "#4F0E99"), na.value = "#000000") +
+  labs(y = "Choice path", x = "Estimated habitat preference for C2") +
+  theme_bw() +
+  theme(
+    line = element_line(colour = palette["coreGrey"]),
+    text = element_text(colour = palette["coreGrey"]),
+    strip.background = element_blank(),
+    strip.text = element_text(face = 4, hjust = 0),
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    panel.grid = element_blank())
+
+
